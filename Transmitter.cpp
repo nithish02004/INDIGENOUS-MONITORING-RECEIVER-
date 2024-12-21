85546245146142ae53d23fe10abba4cd66e387b0
nithish02004 <99329978+nithish02004@users.noreply.github.com>
2024-12-21 09:20:26 +0530

    Update Transmitter.cpp

--- Transmitter.cpp
+++ Transmitter.cpp
@@ -1,42 +1,87 @@
-#include <AESLib.h> // Include AES library for encryption
-
-// AES Encryption Key (16-byte key)
-#define KEY "1234567890123456"
-AESLib aesLib;
-
-// Pins for HC-12
-#define TX_PIN 3 // HC-12 TX
-#define RX_PIN 2 // HC-12 RX
-
-// SoftwareSerial for HC-12 communication
+const int ledPin = 13;
+const int buzzerPin = 12;
+const int ldrPin = A0;
+const int ldrPin2 = A1;
+const String PHONE = "+916382799541";
 #include <SoftwareSerial.h>
-SoftwareSerial hc12(RX_PIN, TX_PIN);
+#include <RH_ASK.h>
+#include <SPI.h> 
+//Alarm reciever's phone number with country code
 
-char encryptedMessage[64]; // Buffer for encrypted data
+//GSM Module RX pin to Arduino 3
+//GSM Module TX pin to Arduino 2
+#define rxPin 3
+#define txPin 2
+SoftwareSerial sim800(rxPin,txPin);
 
-void setup() {
-  Serial.begin(9600);  // Debugging
-  hc12.begin(9600);    // HC-12 baud rate
-  Serial.println("Transmitter Ready");
-}
+RH_ASK rf_driver;
 
-void encryptMessage(const char *plainText, char *encryptedText) {
-  aesLib.encrypt64(plainText, strlen(plainText), encryptedText, KEY);
-}
+void setup () {
+
+  
+    rf_driver.init();
+          delay(1000);
+  Serial.begin(115200);
+  sim800.begin(9600);
+//      delay(1000);
+//  Serial.begin(115200);
+
+  pinMode(ledPin, OUTPUT);
+
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(ldrPin, INPUT);
+    pinMode(ldrPin2, INPUT);
+
+
+//  Serial.println("SIM800L software serial initialize");
+
+  sim800.println("AT");
+      delay(1500);
+      }
 
 void loop() {
-  // Prepare data
-  String message = "Signal Detected at Lat:10.1234 Lon:76.5678";
-  char plainText[64];
-  message.toCharArray(plainText, 64);
+  
+  int ldrStatus = analogRead(ldrPin);  //read the state of the LDR value
+  int ldrStatus2 = analogRead(ldrPin2);
+      const char *msg = "be alert someone came";
+  if (ldrStatus >= 400 &&  ldrStatus2 >=400) {
 
-  // Encrypt the message
-  encryptMessage(plainText, encryptedMessage);
+    noTone(buzzerPin);
+    digitalWrite(ledPin, LOW);    
+//    Serial.println("----------- ALARM off ACTIVATED -----------"); 
+  }
+  else {
+       
+    rf_driver.send((uint8_t *)msg, strlen(msg));
+    rf_driver.waitPacketSent();
+       delay(1500);
+    rf_driver.send((uint8_t *)msg, strlen(msg));
+    rf_driver.waitPacketSent();
+    delay(1500);
+    
+    tone(buzzerPin, 100);
+    digitalWrite(ledPin, HIGH);
+     delay(5000);
 
-  // Send the encrypted message over HC-12
-  hc12.println(encryptedMessage);
-  Serial.print("Sent: ");
-  Serial.println(encryptedMessage);
 
-  delay(5000); // Send every 5 seconds
-}
+    Serial.println("----------- ALARM ACTIVATED -----------"); 
+    sim800.println("AT");
+    delay(500);
+  
+      sim800.println("ATD"+PHONE+";");
+  delay(20000);
+  sim800.println("ATH");
+  delay(1500);
+   sim800.print("AT+CMGF=1\r");
+    delay(500);
+    sim800.print("AT+CMGS=\"+916382799541\"\r");
+    delay(500);
+    sim800.print("SIM800l is working");
+    delay(500);
+    sim800.print((char)26);// (required according to the datasheet)
+    delay(500);
+        noTone(buzzerPin);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+  }
+};
